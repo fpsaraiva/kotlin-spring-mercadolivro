15562f5f54f0ab1034a01786d28871852a59b65c
Fernando Saraiva <fpsaraiva@gmail.com>
2021-10-31 19:22:15 -0300

    criação migration de customer

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,8 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
+	implementation("org.flywaydb:flyway-core:8.0.2")
+
 	runtimeOnly("mysql:mysql-connector-java")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
