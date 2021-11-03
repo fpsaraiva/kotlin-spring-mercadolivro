ffac78efb4029d4a94bd21b0be3cbc2adb2118a8
Fernando Saraiva <fpsaraiva@gmail.com>
2021-11-03 20:41:32 -0300

    adicionando mais validações de customer e livro

--- src/main/kotlin/com/mercadolivro/controller/request/PutCustomerRequest.kt
+++ src/main/kotlin/com/mercadolivro/controller/request/PutCustomerRequest.kt
@@ -1,6 +1,13 @@
 package com.mercadolivro.controller.request
 
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotEmpty
+
 class PutCustomerRequest(
+
+    @field:NotEmpty(message = "nome deve ser informado")
     var name: String,
+
+    @field:Email(message = "email deve ser válido")
     var email: String
 )
