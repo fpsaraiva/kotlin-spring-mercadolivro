f2e1cb0c7d2028843918ac7a2539981f4b6035fd
Fernando Saraiva <fpsaraiva@gmail.com>
2021-11-16 09:38:55 -0300

    testes GenerateNfeListener + uso MockK Static

--- src/main/kotlin/com/mercadolivro/events/listener/GenerateNfeListener.kt
+++ src/main/kotlin/com/mercadolivro/events/listener/GenerateNfeListener.kt
@@ -14,7 +14,7 @@ class GenerateNfeListener(
 
     @Async
     @EventListener
-    fun listener(purchaseEvent: PurchaseEvent) {
+    fun listen(purchaseEvent: PurchaseEvent) {
         val nfe = UUID.randomUUID().toString()
         val purchaseModel = purchaseEvent.purchaseModel.copy(nfe = nfe)
         purchaseService.update(purchaseModel)
